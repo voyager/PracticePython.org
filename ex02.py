bb1e790fb90e8f07027629a4c140a7a01bf1fb7d
voyager <457281+voyager@users.noreply.github.com>
2018-03-03 21:18:48 +0200

    Determining whether a number is even or odd...
    Determining whether a number is even, odd or multiple of 4.

--- ex02.py
+++ ex02.py
@@ -1,6 +1,6 @@
 """Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
 
-Extras:
+Extras: 
 
 1.  If the number is a multiple of 4, print out a different message.
 2.  Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
